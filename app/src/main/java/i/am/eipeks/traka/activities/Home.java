e50d5f71a3148b3296fb8f955f48532d6c9961d0
eipeks <apexzdgr8est@gmail.com>
2017-09-26 12:52:50 +0100

    Another update

--- app/src/main/java/i/am/eipeks/traka/activities/Home.java
+++ app/src/main/java/i/am/eipeks/traka/activities/Home.java
@@ -10,9 +10,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import i.am.eipeks.traka.R;
 import i.am.eipeks.traka.adapters.PagerAdapter;
@@ -32,7 +38,7 @@ public class Home extends AppCompatActivity{
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
         DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        final NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
 
         toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_opened, R.string.drawer_closed);
 
@@ -45,8 +51,25 @@ public class Home extends AppCompatActivity{
         toggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        FirebaseUser userLoggedIn = FirebaseAuth.getInstance().getCurrentUser();
+        if (userLoggedIn != null){
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference("user").child(userLoggedIn.getUid());
+            reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    User user = dataSnapshot.getValue(User.class);
+                    if (user != null){
+                        ((TextView)navigationView.findViewById(R.id.user_full_name)).setText(user.getFullName());
+                        ((TextView)navigationView.findViewById(R.id.user_email)).setText(user.getEmail());
+                    }
+                }
 
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
     }
 
     @Override
